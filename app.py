be0e16b0dd24049fb415240bb654a755d93d0ef9
an-author <onyangomaurice045@outlook.com>
2024-06-19 12:08:31 +0300

    Gifted

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -34,4 +35,5 @@ def login():
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
